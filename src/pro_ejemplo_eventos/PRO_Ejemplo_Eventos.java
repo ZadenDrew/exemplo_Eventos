a301fc0de78cbf2a97f326e32f3a520df5cfff69
Hansen@DESKTOP-P1ST9AA <hansennygard@gmail.com>
2018-04-10 09:51:50 +0200

    Ejemplo Eventos

---
+++ src/pro_ejemplo_eventos/PRO_Ejemplo_Eventos.java
@@ -0,0 +1,14 @@
+package pro_ejemplo_eventos;
+
+/**
+ *
+ * @author abrandarizdominguez
+ */
+public class PRO_Ejemplo_Eventos {
+
+    public static void main(String[] args) {
+        //Evento obj = new Evento();
+        Evento1 obj1 = new Evento1();
+    }
+    
+}
